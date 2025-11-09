2f9d77ccd392e918010abcd2533261a7b2b72a47
Trần Xuân Tiến <tranxuantienvtqs@gmail.com>
2025-11-09 16:46:51 +0700

    update: logo

--- components/navigation/user-footer.tsx
+++ components/navigation/user-footer.tsx
@@ -10,27 +10,27 @@ export default function UserFooter() {
     <footer className="bg-primary text-white mt-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="mb-8 pb-8 border-b border-primary-light flex justify-center">
-          <Image
+          {/* <Image
             src={ASSETS.logo.vfv_logo}
             alt={trans.volleyFederDN}
             width={300}
             height={100}
             className="h-16 w-auto"
-          />
+          /> */}
           <Image
             src={ASSETS.logo.vfd_logo_text}
             alt={trans.volleyFederDN}
             width={300}
             height={100}
-            className="h-16 w-auto ml-15 mr-15"
+            className="h-16 w-auto"
           />
-          <Image
+          {/* <Image
             src={ASSETS.logo.danangcity_logo}
             alt={trans.volleyFederDN}
             width={300}
             height={100}
             className="h-16 w-auto"
-          />
+          /> */}
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8 mb-8">
