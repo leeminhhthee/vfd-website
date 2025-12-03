88987f8ef0e801ffec61e5c622d34abfe2b3fee3
leeminhhthee <minhthe22122003@gmail.com>
2025-12-03 21:27:25 +0700

    registration

--- data/datasource/registration/interactor/registration.interactor.ts
+++ data/datasource/registration/interactor/registration.interactor.ts
@@ -1,10 +1,15 @@
 import { RegistrationStatus } from "@/data/constants/constants";
+import { RegistrationItem } from "@/data/model/registration.model";
 import { registrationRepository } from "../repository/registration.repository";
 
 export const registrationInteractor = {
   async getRegistrationList() {
     const list = await registrationRepository.getRegistrationList();
     return list.sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+  },
+
+  async createRegistration(data: Partial<RegistrationItem>) {
+    return await registrationRepository.createRegistration(data);
   },
 
   async updateRegistrationStatus(id: number, status: RegistrationStatus) {
